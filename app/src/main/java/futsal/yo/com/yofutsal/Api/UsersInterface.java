f74f7fca9643c55dc91e615c74a5fe671fa5c980
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2018-04-18 17:22:11 +0700

    change interface for users

--- app/src/main/java/futsal/yo/com/yofutsal/Api/UsersInterface.java
+++ app/src/main/java/futsal/yo/com/yofutsal/Api/UsersInterface.java
@@ -4,6 +4,7 @@ package futsal.yo.com.yofutsal.Api;
 import futsal.yo.com.yofutsal.Model.ApiResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 
 /**
  * Created by rizalsidikp on 15/04/18.
@@ -11,7 +12,11 @@ import retrofit2.http.GET;
 
 public interface UsersInterface {
 
+//  getAllusers
     @GET("users")
     Call<ApiResponse> getUsers();
 
+//    getUserLogin
+    @GET("user")
+    Call<ApiResponse> getUser();
 }
